9cd05ae610acc33e04369b9e113a3774c9cda981
gleydson-delcho <gleydson.engenhariacivil@gmail.com>
2022-02-05 19:18:16 -0300

    Add server response api, add function register, implements logic visual response data success or error

--- login/src/pages/Login/index.tsx
+++ login/src/pages/Login/index.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../css/login.module.scss';
 
+type Data = {
+  email: string;
+  password: string;
+}
+
 export const Login= () => {
+  
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+    const register = async (event: FormEvent) => {
+      event.preventDefault();
+      const data: Data = {
+        email,
+        password
+      }
+
+    }
+
+
   return (
     <div className="App">
       <div className={styles.loginArea}>
         <h1>LOGIN</h1>
-        <form >
+        <form onSubmit={register}>
           <section className={styles.localName}>
-            <label htmlFor="name">Usuários:</label>
-            <input type="text" name="name" id="name" />
+            <label htmlFor="email">Email:</label>
+            <input type="text" name="email" id="email"  onChange={e => setEmail(e.target.value)}/>
           </section>
           <section className={styles.localPass}>
             <label htmlFor="password">Senha:</label>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" onChange={e => setPassword(e.target.value)} />
           </section>
           <span>
             <Link to="#">esqueci minha senha</Link>
-            <button>login</button>
+            <button type='submit'>login</button>
           </span>
           <Link to="/register">cadastrar</Link>
         </form>
--- login/src/pages/Register/index.tsx
+++ login/src/pages/Register/index.tsx
@@ -1,28 +1,87 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Styles from '../../css/register.module.scss';
+import { api } from '../../services/api';
 
-import Styles  from '../../css/register.module.scss';
+type Values = {
+  name: string;
+  email: string;
+  password: string;
+}
 
 export const Register: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [isNotCreate, setIsNotCreate] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [passwordPass, setPasswordPass] = useState<string>('');
+  const [repeatPass, setRepeatPass] = useState<string>('');
+
+  const register = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const password = passwordPass;
+    if (passwordPass === repeatPass) {
+      const registerValues: Values = {
+        name,
+        email,
+        password
+      }
+      
+      setIsActive(true);
+      await api.post("/users", registerValues);
+      setTimeout(() => {
+        setIsActive(false)
+        navigate('/')
+      }, 2000);
+      }else {
+
+        setIsNotCreate(true);
+        setTimeout(() => {
+          setIsNotCreate(false)
+        }, 2000);
+      }
+    
+  }
+
   return (
 
-    <div className={Styles.registerForm}>
-      <h2>Formulário de cadastro</h2>
-      <form >
-        <label htmlFor="name">Nome:</label>
-        <input type="text" />
+    <>
+      {
+        isActive &&
+        <div className={Styles.success}>
+          <p>Salvo com sucesso!</p>
+        </div>
+      }
+      {
+        isNotCreate &&
+        <div className={Styles.error}>
+          <p>Os dados não foram salvos!</p>
+        </div>
+      }
+      <div className={Styles.registerForm}>
+        <h2>Formulário de cadastro</h2>
+        <form onSubmit={register}>
+          <label htmlFor="name">Nome:</label>
+          <input name="name" type="text" required onChange={e => { setName(e.target.value) }} />
 
-        <label htmlFor="email">E-mail:</label>
-        <input type="text" />
+          <label htmlFor="email">E-mail:</label>
+          <input name="email" type="text" required onChange={e => { setEmail(e.target.value) }} />
 
-        <label htmlFor="pass">Senha</label>
-        <input type="password" />
+          <label htmlFor="pass">Senha</label>
+          <input name="password" type="password" required onChange={e => { setPasswordPass(e.target.value) }} />
 
-        <label htmlFor="Repeatpass">Repetir senha</label>
-        <input type="password" />
+          <label htmlFor="Repeatpass">Repetir senha</label>
+          <input name="repeatPassword" type="password" required onChange={e => { setRepeatPass(e.target.value) }} />
 
-        <button>Enviar</button>
-      </form>
-    </div>
+          <button type="submit" onClick={() => register}>Cadastrar</button>
+        </form>
+      </div>
+    </>
 
   );
 }
